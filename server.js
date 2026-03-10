d6abdd0edd9546d1fd8018a4ae7cda5665801a2f
Aaditya-Patil29 <patilaaditya@347gmail.com>
2026-03-10 22:48:42 +0530

    postman

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const express = require('express');
+const app = require('./src/app');   
 
 
-const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 })
-
-app.get('/about', (req, res) => {
-    res.send('This is the about page')
-})
-
-app.listen(3000)
